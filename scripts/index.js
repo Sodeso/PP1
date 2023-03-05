cd363fa3c7148d9c506c68731e97c58825018719
Eugene <75364129+Sodeso@users.noreply.github.com>
2023-03-06 00:06:48 +0300

    +add datatable fix bugs, update dependencies

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,24 @@
 
-/*
-$(document).ready(function() {
-    var id_select = $('.js-example-basic-single').select2().val
-    console.log(id_select);
-});
 
-if ( ! DataTable.isDataTable( '#example' ) ) {
-    new DataTable('#example');
-  }
-*/
+var data = [
+  [
+    "Tiger Nixon",
+    "System Architect",
+    "Edinburgh",
+    "5421",
+    "2011/04/25",
+    "$3,120"
+  ],
+  [
+    "Garrett Winters",
+    "Director",
+    "Edinburgh",
+    "8422",
+    "2011/07/25",
+    "$5,300"
+  ]
+]
+
+$('#example').DataTable( {
+  data: data
+} );
