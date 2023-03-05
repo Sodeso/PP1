63014fa6d998db0584f39362b7ffc9a9582fc4e3
Eugene <75364129+Sodeso@users.noreply.github.com>
2023-03-05 19:33:46 +0300

    in progress

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,12 @@
-import "./bootstrap";
-import $ from "jquery";
-import select2 from 'select2';
+//ниче не работает, хз почему
+import select2 from '../node_modules/select2/dist/js/select2';
 select2();
 
 $(document).ready(function() {
     var id_select = $('.js-example-basic-single').select2().val
     console.log(id_select);
 });
+
+if ( ! DataTable.isDataTable( '#example' ) ) {
+    new DataTable('#example');
+  }
