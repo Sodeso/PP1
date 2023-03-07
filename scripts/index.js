9594c48497fa21954024cce1de23844ed8ad2327
Eugene <75364129+Sodeso@users.noreply.github.com>
2023-03-07 14:06:13 +0300

    +add owl carousel

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 
-
+//данные для таблицы
 var data = [
   [
     "Tiger Nixon",
@@ -19,6 +19,48 @@ var data = [
   ]
 ]
 
-$('#example').DataTable( {
+//вызов owl carousel
+$(document).ready(function () {
+  var owl = $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    items: 3,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:false,
+    autoplaySpeed:1000,
+    onInitialize: function (event) {
+      if ($('.owl-carousel .item').length <= 5) {
+        this.settings.loop = false;
+      }
+      
+    }
+  })
+});
+
+/*naw owl carousel 
+$('.play').on('click',function(){
+  owl.trigger('autoplay.play.owl',[1000])
+})
+$('.stop').on('click',function(){
+  owl.trigger('autoplay.stop.owl')
+})
+*/
+
+
+//вызов select2
+$(document).ready(function () {
+  $('.js-example-basic-single').select2();
+
+});
+
+//вызов datatable
+$('#example').DataTable({
   data: data
-} );
+});
+
+//select2 типо выбор языка
+$('#mySelect2').on('select2:select', function (e) {
+  console.log("id:" + e.params.data.id + "| text:" + e.params.data.text);
+});
